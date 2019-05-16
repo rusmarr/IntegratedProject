50ca7c62cc23bba80b6a20a5d35835e9514e9c1b
rusmarr <35305575+rusmarr@users.noreply.github.com>
2019-05-16 12:05:33 -0500

    Add files via upload

---
+++ Concrete/DefaultConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Buttons.Domain.Entities;
+using System.Data.Entity;
+
+namespace Buttons.Domain.Concrete
+{
+    public class DefaultConnection : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
