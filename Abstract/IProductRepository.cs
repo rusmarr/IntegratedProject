6f47725f9e88be0a1852c0e9756679fbd7a9770d
rusmarr <35305575+rusmarr@users.noreply.github.com>
2019-05-16 12:03:50 -0500

    Add files via upload

---
+++ Abstract/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Buttons.Domain.Entities;
+
+namespace Buttons.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+
+        Product DeleteProduct(int ProductID);
+    }
+}
