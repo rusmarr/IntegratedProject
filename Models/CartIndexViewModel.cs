6800ec87f9bb005ebe739c164864bc835bd0bdbf
rusmarr <35305575+rusmarr@users.noreply.github.com>
2019-05-16 12:08:49 -0500

    Add files via upload

---
+++ Models/CartIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Buttons.Domain.Entities;
+
+namespace Buttons.WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ Models/ProductsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Buttons.Domain.Entities;
+namespace Buttons.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
